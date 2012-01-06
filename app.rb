d9c5798fb6cb8c32304032a3a90da267d008db3e
Max Katz <maxwelljkatz@gmail.com>
2012-01-06 16:44:23 -0700

    set up ARGV, passed it to instance variable, instantiation passed instance variable to initialize method

--- app.rb
+++ app.rb
@@ -1,8 +1,27 @@
 class TwitterArchiver
 
-	def initialize(twitter_handle)
-		user_data = JSON.parse(`curl search.twitter.com/search.json?q=#{twitter_handle}`)
-		newfile = File.new("#{twitter_handle}_info.txt", "w")
-		newfile.write(user_data)
+	require 'JSON'
+
+	def initialize(user_name)
+		@user_name = user_name
 	end
+
+
+	def getTwitterData
+		@user_data = JSON.parse(`curl search.twitter.com/search.json?q=#{@user_name}`)
+		puts @user_data
+	end
+	
+	def saveData
+		@newFile = File.new("#{a}_info.txt", "w")
+		@newFile.write()
+	end
+
 end
+
+ARGV.each do |a|
+	@user_name = a
+end
+
+t1 = TwitterArchiver.new(@user_name)
+t1.getTwitterData()
