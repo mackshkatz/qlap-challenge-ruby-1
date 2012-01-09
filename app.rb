b55e8cb101f056cc48e9b6fff3cf53c054bf6f05
Max Katz <maxwelljkatz@gmail.com>
2012-01-08 19:59:22 -0700

    Finished basic app challenge, now onto the bonuses

--- app.rb
+++ app.rb
@@ -1,5 +1,4 @@
 class TwitterArchiver
-
 	require 'JSON'
 
 	def initialize(user_name)
@@ -9,19 +8,16 @@ class TwitterArchiver
 
 	def getTwitterData
 		@user_data = JSON.parse(`curl search.twitter.com/search.json?q=#{@user_name}`)
-		puts @user_data
 	end
 	
 	def saveData
-		@newFile = File.new("#{a}_info.txt", "w")
-		@newFile.write()
+		@newFile = File.new("#{@user_name}_info.txt", "w")
+		@newFile.write(@user_data)
 	end
-
 end
 
-ARGV.each do |a|
-	@user_name = a
-end
+user_name = ARGV[0]
 
-t1 = TwitterArchiver.new(@user_name)
+t1 = TwitterArchiver.new(user_name)
 t1.getTwitterData()
+t1.saveData()
