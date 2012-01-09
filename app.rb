f68b12cd5f21e784d77c9c6552f4fd58640b4a1a
Max Katz <maxwelljkatz@gmail.com>
2012-01-09 10:54:18 -0700

    Ran my script to show the files it produces, right now just using the Twitter API's most basic GET search query, by passing my script a twitter handle, ex: @twitter_handle.

--- app.rb
+++ app.rb
@@ -5,7 +5,6 @@ class TwitterArchiver
 		@user_name = user_name
 	end
 
-
 	def getTwitterData
 		@user_data = JSON.parse(`curl search.twitter.com/search.json?q=#{@user_name}`)
 	end
@@ -21,3 +20,6 @@ user_name = ARGV[0]
 t1 = TwitterArchiver.new(user_name)
 t1.getTwitterData()
 t1.saveData()
+
+# t2 = TwitterArchiver.new("one")
+# t3 = TwitterArchiver.new("two")
